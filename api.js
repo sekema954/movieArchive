865d6ad753653d0a4fdc3a083cab04bf55526bde
sekema954 <sekema954@gmail.com>
2024-06-03 12:13:01 -0400

    initial commit

---
+++ api.js
@@ -0,0 +1,128 @@
+getResults();
+
+async function getResults() {
+    const searchInput = document.querySelector(".search-div");
+    const searchValue = searchInput.value;
+    const url = `https://api.jikan.moe/v4/anime?q=${searchValue}`;
+    const options = {
+        method: 'GET',
+        header: ''
+    }
+
+    try {
+        console.clear();
+        const request = await fetch(url, options);
+        const result = await request.json();
+        console.log(result);
+
+        // Select and clear movie wrapper
+        const movieWrapper = document.querySelector(".movie-card-wrapper"); 
+        movieWrapper.innerHTML = "";    
+        
+
+        // Loop over the results and create movie cards for each result
+        for (const movie of result.data) {
+            const movieBox = document.createElement('div');
+            movieBox.classList.add('movie-card');
+
+            // Movie poster
+            const movieIcon = document.createElement('div');
+            movieIcon.classList.add('movie-bg');
+            const movieImg = document.createElement('img');
+
+            // Access the image URL based on the actual structure of the API response
+            const imageUrl = movie.images && movie.images.webp && movie.images.webp.large_image_url;
+            if (imageUrl) {
+                movieImg.src = imageUrl;
+            } else {
+                // If the image URL is not available, provide a fallback image
+                movieImg.src = '';
+            }
+
+            movieIcon.appendChild(movieImg);
+            movieBox.appendChild(movieIcon);
+            
+            // Append the movieBox to the movieWrapper
+            movieWrapper.appendChild(movieBox);
+
+            movieImg.addEventListener('click', () => {
+                // Handle the click event here, for example, display a message
+                console.log(`you clicked on ${movie.title}`)
+                //change movie title
+                const mainTitle = document.querySelector('.title-fetch');
+                mainTitle.innerHTML = movie.title;
+
+                //movie sypnosis
+                const sypnosis = document.querySelector(".sypnosis-fetch");
+                sypnosis.innerHTML = movie.synopsis;
+
+                //convert year-month-day-time to year
+                const dateString = movie.aired.from;
+                const parts = dateString.split("-");
+                const year = parts[0];
+                console.log(`Year: ${year}`);
+
+                //update aired year
+                const airedYear = document.querySelector(".aired-fetch");
+                airedYear.innerHTML = year;
+
+                //show number of episodes
+                const episodes = document.querySelector(".episode-fetch");
+                if(movie.episodes === null) {
+                    episodes.innerHTML = "Still releasing."
+                } else {
+                    episodes.innerHTML = movie.episodes;
+                    console.log(`This show has ${movie.episodes} episodes.`)
+                };
+               
+                
+
+
+
+                //watch trailer
+                const trailerURL = movie.trailer.embed_url;
+                const trailerBtn = document.querySelector('.trailer-fetch');
+
+                //add click event to trialer btn
+                trailerBtn.addEventListener("click", (event)=>{
+                    event.preventDefault();
+                    if(trailerURL === 'null') {
+                        alert("No trailer links found!");
+                    } else {
+                        window.open(trailerURL, '_blank');
+                        console.log(`You clicked on ${trailerURL}`);
+                    }
+                });
+
+                //change background
+                const posterBg = document.querySelector(".poster-fetch");
+                posterBg.src = imageUrl;
+
+                //scrollTop
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                });
+
+
+                //type fetch
+                const type = document.querySelector(".type-fetch");
+                type.innerHTML = movie.type;
+
+
+                //change overview main background
+                const overviewContainer = document.querySelector(".overview");
+                overviewContainer.style.background = `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url(${imageUrl})`;
+                overviewContainer.style.backgroundPosition = 'center';
+                overviewContainer.style.backgroundSize = 'cover';
+                overviewContainer.style.backgroundRepeat = 'no-repeat';
+            });
+        }
+
+        
+
+    } catch(error) {
+        console.error(error);
+    }
+
+};
--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-//slider for signup page
-//select wrappers
-function changeSlide(){
-    const step1 = document.querySelector(".card1");
-    const step2 = document.querySelector(".card2");
-    const step3 = document.querySelector(".card3");
-    const setupWrapper = document.querySelectorAll(".setup-wrapper");
-    
-    //select buttons
-    const btn1 = document.querySelector(".step-btn1");
-    const btn2 = document.querySelector(".step-btn2");
-    const btn3 = document.querySelector(".step-btn3");
-    
-    //change display for step-wrappers
-    btn1.addEventListener("click", ()=>{
-        step1.style.display ='none';
-        step2.style.display = 'inline-block';
-    });
-    
-    btn2.addEventListener("click", ()=>{
-        step2.style.display = "none";
-        step3.style.display = 'inline-block';
-    });
+//menubar function
+
+
+menuIconClicked()
+function menuIconClicked(){
+    const menubar = document.querySelector(".menu-bar");
+    const dropdownMenu = document.querySelector(".dropdown-card")
+    const dropdownWrapper = document.querySelector(".dropdown-ul");
+    menubar.addEventListener("click", ()=>{
+        menubar.classList.toggle('change');
+        dropdownMenu.classList.toggle("dropdown-card-classlist");
+        dropdownWrapper.classList.toggle("show-display");
+    })
 };
-changeSlide();
-
-
